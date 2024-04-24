40801fe0da7bc75a041659b5710b3813312a020f
BJ505 <byronjaramilloquezada@gmail.com>
2024-04-24 18:46:17 -0400

    Hateoas and exceptions
    uso de Hateoas y control de excepciones básicas

--- src/main/java/com/example/publicacion/controller/PublicacionController.java
+++ src/main/java/com/example/publicacion/controller/PublicacionController.java
@@ -1,5 +1,9 @@
 package com.example.publicacion.controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.publicacion.model.Comentarios;
@@ -9,6 +13,8 @@ import com.example.publicacion.service.PublicacionService;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -27,31 +33,66 @@ public class PublicacionController {
     private ComentarioService comentarioService;
 
     @GetMapping
-    public List<Publicacion> getAllPublicaciones() {
-        return publicacionService.getPublicaciones();
+    public CollectionModel<EntityModel<Publicacion>> getAllPublicaciones() {
+        List<Publicacion> publicaciones = publicacionService.getPublicaciones();
+        List<EntityModel<Publicacion>> publicacionResorurces = publicaciones.stream()
+            .map( publicacion -> EntityModel.of(publicacion,
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPublicacionId(publicacion.getId())).withSelfRel()
+            ))
+            .collect(Collectors.toList());
+
+        WebMvcLinkBuilder linkTo = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPublicaciones());
+        CollectionModel<EntityModel<Publicacion>> resources = CollectionModel.of(publicacionResorurces, linkTo.withRel("publicaciones"));
+
+        return resources;
     }
 
     //---------PUBLICACIONES-------------
-    //Traer todas las publicaciones (solo campos de la tabla PUBLICACIONES)
+    //Traer todas las publicaciones
     @GetMapping("/publicaciones")
-    public List<Publicacion> getPublicaciones() {
-        return publicacionService.getPublicaciones();
+    public CollectionModel<EntityModel<Publicacion>> getPublicaciones() {
+        List<Publicacion> publicaciones = publicacionService.getPublicaciones();
+        List<EntityModel<Publicacion>> publicacionResorurces = publicaciones.stream()
+            .map( publicacion -> EntityModel.of(publicacion,
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPublicacionId(publicacion.getId())).withSelfRel()
+            ))
+            .collect(Collectors.toList());
+
+        WebMvcLinkBuilder linkTo = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPublicaciones());
+        CollectionModel<EntityModel<Publicacion>> resources = CollectionModel.of(publicacionResorurces, linkTo.withRel("publicaciones"));
+
+        return resources;
     }
 
     //Traer una publicacion (por Id)
     @GetMapping("/publicaciones/{id}")
-    public Optional<Publicacion> getPublicacionId(@PathVariable Long id) {
-        return publicacionService.getPublicacionId(id);
+    public EntityModel<Publicacion> getPublicacionId(@PathVariable Long id) {
+        Optional<Publicacion> publicacion = publicacionService.getPublicacionId(id);
+
+        if(publicacion.isPresent()){
+            return EntityModel.of(publicacion.get(),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPublicacionId(id)).withSelfRel(),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPublicaciones()).withRel("publicaciones"));
+        } else{
+            throw new PublicacionNotFoundException("Publicacion no encontrada con el id: " + id);
+        }
     }
 
     @PostMapping("/publicaciones")
-    public Publicacion createPublicacion(@RequestBody Publicacion publicacion) {
-        return publicacionService.createPublicacion(publicacion);
+    public EntityModel<Publicacion> createPublicacion(@Validated @RequestBody Publicacion publicacion) {
+        Publicacion createPublicacion = publicacionService.createPublicacion(publicacion);
+        return EntityModel.of(createPublicacion,
+        WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPublicacionId(createPublicacion.getId())).withSelfRel(),
+        WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPublicaciones()).withRel("publicaciones"));
     }
 
     @PutMapping("/publicaciones/{id}")
-    public Publicacion updatePublicacion(@PathVariable Long id, @RequestBody Publicacion publicacion) {
-        return publicacionService.updatePublicacion(id, publicacion);
+    public EntityModel<Publicacion> updatePublicacion(@PathVariable Long id, @RequestBody Publicacion publicacion) {
+        Publicacion updatePublicacion = publicacionService.updatePublicacion(id, publicacion);
+
+        return EntityModel.of(updatePublicacion,
+        WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPublicacionId(updatePublicacion.getId())).withSelfRel(),
+        WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPublicaciones()).withRel("publicaciones"));
     }
     
     @DeleteMapping("/publicaciones/{id}")
@@ -63,24 +104,47 @@ public class PublicacionController {
     //---------COMENTARIOS-------------
     //Traer todos los comentarios (solo campos de la tabla COMENTARIOS)
     @GetMapping("/comentarios")
-    public List<Comentarios> getComentarios() {
-        return comentarioService.getComentarios();
+    public CollectionModel<EntityModel<Comentarios>> getComentarios() {
+        List<Comentarios> comentarios = comentarioService.getComentarios();
+        List<EntityModel<Comentarios>> comentariosResources = comentarios.stream()
+            .map( comentario -> EntityModel.of(comentario,
+            WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getComentariosId(comentario.getId())).withSelfRel()
+        ))
+        .collect(Collectors.toList());
+        WebMvcLinkBuilder linkTo = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getComentarios());
+        CollectionModel<EntityModel<Comentarios>> resources = CollectionModel.of(comentariosResources, linkTo.withRel("comentarios"));
+
+        return resources;
     }
 
     //Traer un solo comentario (por Id)
     @GetMapping("/comentarios/{id}")
-    public Optional<Comentarios> getComentarios(@PathVariable Long id) {
-        return comentarioService.getComentarioId(id);
+    public EntityModel<Comentarios> getComentariosId(@PathVariable Long id) {
+        Optional<Comentarios> comentario = comentarioService.getComentarioId(id);
+
+        if(comentario.isPresent()){
+            return EntityModel.of(comentario.get(),
+            WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getComentariosId(id)).withSelfRel(),
+            WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getComentarios()).withRel("comentarios"));
+        }else{
+            throw new PublicacionNotFoundException("Comentario no encontrado con el id: " + id);
+        }
     }
 
     @PostMapping("/comentarios/{idPublicacion}")
-    public Comentarios createComentario(@PathVariable Long idPublicacion,@RequestBody Comentarios comentarios) {
-        return comentarioService.createComentario(idPublicacion,comentarios);
+    public EntityModel<Comentarios> createComentario(@PathVariable Long idPublicacion,@Validated @RequestBody Comentarios comentarios) {
+        Comentarios createComentario = comentarioService.createComentario(idPublicacion,comentarios);
+        return EntityModel.of(createComentario,
+        WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPublicacionId(createComentario.getId())).withSelfRel(),
+        WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPublicaciones()).withRel("comentarios"));
     }
 
     @PutMapping("/comentarios/{idComentario}/{idPublicacion}")
-    public Comentarios updateComentario(@PathVariable Long idComentario,@PathVariable Long idPublicacion, @RequestBody Comentarios comentarios) {
-        return comentarioService.updateComentario(idComentario,idPublicacion, comentarios);
+    public EntityModel<Comentarios> updateComentario(@PathVariable Long idComentario,@PathVariable Long idPublicacion, @RequestBody Comentarios comentarios) {
+        Comentarios comentarioUpdate = comentarioService.updateComentario(idComentario, idPublicacion, comentarios);
+        return EntityModel.of(comentarioUpdate,
+        WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPublicacionId(comentarioUpdate.getId())).withSelfRel(),
+        WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPublicaciones()).withRel("comentarios"));
     }
 
     @DeleteMapping("/comentarios/{id}")
